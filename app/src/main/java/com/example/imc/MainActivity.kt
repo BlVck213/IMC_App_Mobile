5a30aaedcd6be9bbdfe478613c89ad9b950daef4
BlVck213 <21192922@CFP127ALUNO.LOCAL>
2021-08-23 11:13:10 -0300

    001

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+
+        val editTextPeso = findViewById<EditText>(R.id.edit_text_peso)
+        val editTextAltura = findViewById<EditText>(R.id.edit_text_altura)
+
+        val textImc = findViewById<TextView>(R.id.textViewImc)
+
+        val textStatus = findViewById<TextView>(R.id.textViewStatus)
+
+
+
+            buttonCalcular.setOnClickListener {
+                textImc.text = calcularImc(
+                    editTextPeso.text.toString().toInt(),
+                    editTextAltura.text.toString().toDouble()
+                ).toString()
+
+
+            }
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/imc/imc.kt
@@ -0,0 +1,13 @@
+package com.example.imc
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+
+fun calcularImc(peso: Int, altura: Double) : Double {
+    val imc = peso / (altura * altura)
+    return imc.toBigDecimal().setScale(1,RoundingMode.UP).toDouble()
+}
+
+
+
